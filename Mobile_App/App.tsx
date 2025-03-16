3017e397b4416d1dde5878c74b8220218883f160
Kithmina Adithya <adithyakithmina2006@gmail.com>
2025-03-16 18:16:22 +0530

    FUlly updates weather UI

--- Mobile_App/App.tsx
+++ Mobile_App/App.tsx
@@ -16,6 +16,23 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
+<<<<<<< Updated upstream
+=======
+        <Stack.Screen name="CreatePost" component={CreatePostScreen}options={{ headerShown: false }}/>
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Signup" component={SignupScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="MainHome" component={HomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="CropSelection" component={CropSelectionScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Detection" component={DetectionScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Solution" component={SolutionScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="DailyRates" component={DailyRatesScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="UserProfile" component={UserProfileScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="EditProfile" component={EditProfileScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Community" component={CommunityScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Comments" component={CommentsScreen} options={{ headerShown: false }} />
+        
+
+>>>>>>> Stashed changes
         <Stack.Screen 
           name="Login" 
           component={LoginScreen}
