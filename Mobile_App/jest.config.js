13e44eee69cd76b8701aed3729ba96531e625ae3
Poorna Madusith <157389469+poorna-madusith@users.noreply.github.com>
2025-03-22 07:34:17 +0530

    fix onfiguration error ci

--- Mobile_App/jest.config.js
+++ Mobile_App/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: 'react-native',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFiles: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
     'node_modules/(?!((@react-native|react-native|@react-navigation|@react-native-community)/.*))'
